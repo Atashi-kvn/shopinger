32fc0f0347dc6f30f99f11ef904df29e93cf2efb
Kelvin Atandi <ataskv.gov@gmail.com>
2023-12-19 16:03:50 +0300

    Connecting intents

--- app/src/main/java/com/atasksolutions/shopinger/MainActivity.kt
+++ app/src/main/java/com/atasksolutions/shopinger/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.atasksolutions.shopinger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginBtn: Button = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+        }
+
+        val joinBtn: Button = findViewById(R.id.join_btn)
+
+        joinBtn.setOnClickListener {
+            val intent = Intent(this, RegActivity::class.java)
+        }
     }
 }
